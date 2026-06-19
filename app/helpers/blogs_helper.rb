00c0a6bcfb50f7e8853c40e47ddfc5faa35e2661
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-19 11:04:33 +0900

    format_contentをsimple_formatを使うよう修正してrubocop:disableを撤去

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,6 @@
 
 module BlogsHelper
   def format_content(blog)
-    ERB::Util.html_escape(blog.content).gsub("\n", '<br>').html_safe # rubocop:disable Rails/OutputSafety
+    simple_format(blog.content)
   end
 end
